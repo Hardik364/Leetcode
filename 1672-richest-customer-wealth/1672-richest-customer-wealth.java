441a499eabef1e94b204ce672feeefda9d64fa85
Hardik Bajaj <121892479+Hardik364@users.noreply.github.com>
2024-11-26 20:39:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (35.18%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,14 +1,20 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
+        // varaible to store the richest Customer Wealth
         int richest = 0;
+
+        // Loop for going through the Row
         for(int row = 0; row<accounts.length; row++)
         {
+            // Variable to store every row sum once
             int temp = 0;
             
+            // Going through the columns
             for(int col = 0; col<accounts[row].length; col++)
             {
                 temp = temp + accounts[row][col];
             }
+            // checking if the previous is greater or not 
             if(temp > richest)
             {
                 richest = temp;
