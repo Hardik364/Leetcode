a7d63dfe9cc0bd627561249b96c4b29a85df0262
Hardik Bajaj <121892479+Hardik364@users.noreply.github.com>
2024-12-04 22:55:30 +0530

    Time: 0 ms (100%), Space: 42.9 MB (43.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,25 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int count = 0;
-        for(int i = 0; i<nums.length; i++)
-        {
-            if(nums[i] == target)
-            {
-                count++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] >= target){
                 return i;
             }
         }
-
-        if(count == 0)
-        {
-            for(int i = nums.length-1; i>=0; i--)
-            {
-                if(nums[i] < target)
-                {
-                    return i+1;
-                }
-            }
-        }
-        return 0;
+        return nums.length;
     }
 }
