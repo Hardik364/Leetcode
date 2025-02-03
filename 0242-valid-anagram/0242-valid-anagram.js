4ff80d3e0cd90276e868e321d2be5ee85cdd9ae7
Hardik Bajaj <121892479+Hardik364@users.noreply.github.com>
2025-02-03 12:08:52 +0530

    Time: 31 ms (21.99%), Space: 54.5 MB (21.33%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const str1 = s.split('').sort().join('');
+    const str2 = t.split('').sort().join('');
+
+    if(str1.length != str2.length)
+    {
+        return false;
+    }
+    if(str1 == str2)
+    {
+        return true;
+    }
+    return false;
+};
