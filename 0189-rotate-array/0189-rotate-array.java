cff71f1e1befd1e7857e078d7c5927840760980d
Hardik Bajaj <121892479+Hardik364@users.noreply.github.com>
2025-03-15 14:39:36 +0530

    Time: 0 ms (100%), Space: 57.7 MB (13.84%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,22 +1,39 @@
 class Solution {
+    public void Reverse(int[] nums, int i, int j)
+    {
+        while(i<j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        int[] arr = new int[n];
+        // APPROACH 1
+        Reverse(nums,0,n-k-1);
+        Reverse(nums,n-k,n-1);
+        Reverse(nums,0,n-1);
+        // APPROACH 2
+        // int[] arr = new int[n];
 
-        for(int i = 0; i < k; i++)
-        {
-            arr[i] = nums[n-k+i];
-        }
+        // for(int i = 0; i < k; i++)
+        // {
+        //     arr[i] = nums[n-k+i];
+        // }
 
-        for(int i = k; i<n; i++)
-        {
-            arr[i] = nums[i-k];
-        }
+        // for(int i = k; i<n; i++)
+        // {
+        //     arr[i] = nums[i-k];
+        // }
 
-        for(int i = 0; i<n; i++)
-        {
-            nums[i] = arr[i];
-        }
+        // for(int i = 0; i<n; i++)
+        // {
+        //     nums[i] = arr[i];
+        // }
+
     }
 }
