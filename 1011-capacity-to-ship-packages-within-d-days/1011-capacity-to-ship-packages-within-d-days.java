a358d27a75ffa98723505fbaabd94ec0fcadd8d7
Hardik Bajaj <121892479+Hardik364@users.noreply.github.com>
2025-05-13 14:53:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,54 @@
+class Solution {
+    public Boolean isPossible(int mid, int[] arr, int days)
+    {
+        Boolean flag = true;
+        int sum = 0;
+        int d = 0;
+        for(int i = 0; i<arr.length; i++)
+        {
+            if(arr[i] > mid)
+            {
+                return false;
+            }
+            if(sum + arr[i] <= mid)
+            {
+                sum = sum + arr[i];
+            }
+            else 
+            {
+                d++;
+                sum = arr[i];
+                if(d>days)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] arr, int days) {
+        int low = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++)
+        {
+            low = Math.max(low,arr[i]);
+            sum = sum + arr[i];
+        }
+        int high = sum;
+        int ans = 0;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(isPossible(mid,arr,days))
+            {
+                ans = mid;
+                high = mid - 1; 
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
